89077729192fd3efd8f9b9338f11f6941be09c5f
uga-rosa <uga6603@gmail.com>
2023-05-30 01:28:23 +0900

    Separate handlers

--- denops/@ddu-sources/nvim_lsp.ts
+++ denops/@ddu-sources/nvim_lsp.ts
@@ -7,6 +7,14 @@ import { Denops } from "https://deno.land/x/ddu_vim@v2.8.6/deps.ts#^";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.4.1/file.ts#^";
 import { Location, LocationLink } from "npm:vscode-languageserver-types@3.17.3";
 
+const SUPPORTED_METHODS = {
+  ["textDocument/declaration"]: "textDocument/declaration",
+  ["textDocument/definition"]: "textDocument/definition",
+  ["textDocument/typeDefinition"]: "textDocument/typeDefinition",
+  ["textDocument/implementation"]: "textDocument/implementation",
+  ["textDocument/references"]: "textDocument/references",
+};
+
 function toLocation(loc: Location | LocationLink): Location {
   if ("uri" in loc && "range" in loc) {
     return loc;
@@ -42,24 +50,24 @@ function isDenoUriWithFragment(location: Location) {
 
 type Params = {
   method: string;
+  autoJump: boolean;
 };
 
-const SUPPORTED_METHODS = {
-  ["textDocument/declaration"]: "textDocument/declaration",
-  ["textDocument/definition"]: "textDocument/definition",
-  ["textDocument/typeDefinition"]: "textDocument/typeDefinition",
-  ["textDocument/implementation"]: "textDocument/implementation",
-  ["textDocument/references"]: "textDocument/references",
-};
+type Response = {
+  clientId: number;
+  result: unknown;
+}[];
 
 export class Source extends BaseSource<Params> {
   kind = "file";
+
   gather(args: {
     denops: Denops;
     sourceParams: Params;
     context: Context;
   }): ReadableStream<Item<ActionData>[]> {
-    const { denops, sourceParams: { method }, context } = args;
+    const { denops, sourceParams: { method, autoJump }, context } = args;
+    const { definitionHandler, referencesHandler } = this;
 
     return new ReadableStream({
       async start(controller) {
@@ -71,7 +79,7 @@ export class Source extends BaseSource<Params> {
 
         const response = await denops.eval(
           `luaeval("require'ddu_nvim_lsp'['${method}'](${context.bufNr}, ${context.winId})")`,
-        ) as { clientId: number; result: unknown }[] | null;
+        ) as Response | null;
 
         if (response === null) {
           controller.close();
@@ -83,60 +91,15 @@ export class Source extends BaseSource<Params> {
           case SUPPORTED_METHODS["textDocument/definition"]:
           case SUPPORTED_METHODS["textDocument/typeDefinition"]:
           case SUPPORTED_METHODS["textDocument/implementation"]: {
-            const locationClientIdPairs: {
-              location: Location;
-              clientId: number;
-            }[] = [];
-
-            const locations = response.flatMap(({ result, clientId }) => {
-              /**
-               * response.result: Location | Location[] | LocationLink[]
-               * References:
-               * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_declaration
-               * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_definition
-               * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_typeDefinition
-               * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_implementation
-               */
-              const locations = (Array.isArray(result) ? result : [result]) as
-                | Location[]
-                | LocationLink[];
-
-              return locations.map((loc) => {
-                const location = toLocation(loc);
-                locationClientIdPairs.push({ location, clientId });
-                return location;
-              });
-            }).filter((location) => {
-              return !isDenoUriWithFragment(location);
-            });
-
-            if (locations.length === 1) {
-              // Jump directly when there is only one candidate.
-              const pairs = locationClientIdPairs
-                .find(({ location }) => locations[0] === location);
-              const clientId = pairs?.clientId as number;
-              await denops.eval(
-                `luaeval("require'ddu_nvim_lsp'.jump(_A.location, _A.clientId)", l:)`,
-                { location: locations[0], clientId },
-              );
-            } else {
-              const items = locations.map(locationToItem);
+            const items = await definitionHandler(denops, response, autoJump);
+            if (items) {
               controller.enqueue(items);
             }
-
             break;
           }
           case SUPPORTED_METHODS["textDocument/references"]: {
-            const items = response.flatMap(({ result }) => {
-              /**
-               * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_references
-               */
-              const locations = result as Location[];
-              return locations;
-            }).filter((location) => !isDenoUriWithFragment(location))
-              .map(locationToItem);
+            const items = referencesHandler(response);
             controller.enqueue(items);
-
             break;
           }
         }
@@ -146,9 +109,72 @@ export class Source extends BaseSource<Params> {
     });
   }
 
+  async definitionHandler(
+    denops: Denops,
+    response: Response,
+    autoJump: boolean,
+  ): Promise<Item<ActionData>[] | undefined> {
+    const locationClientIdPairs: {
+      location: Location;
+      clientId: number;
+    }[] = [];
+
+    const locations = response.flatMap(({ result, clientId }) => {
+      /**
+       * response.result: Location | Location[] | LocationLink[]
+       * References:
+       * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_declaration
+       * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_definition
+       * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_typeDefinition
+       * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_implementation
+       */
+      const locations = (Array.isArray(result) ? result : [result]) as
+        | Location[]
+        | LocationLink[];
+
+      return locations.map((loc) => {
+        const location = toLocation(loc);
+        if (autoJump) {
+          locationClientIdPairs.push({ location, clientId });
+        }
+        return location;
+      });
+    }).filter((location) => {
+      return !isDenoUriWithFragment(location);
+    });
+
+    if (autoJump && locations.length === 1) {
+      // Jump directly when there is only one candidate.
+      const pair = locationClientIdPairs
+        .find(({ location }) => locations[0] === location);
+      const clientId = pair?.clientId as number;
+      await denops.eval(
+        `luaeval("require'ddu_nvim_lsp'.jump(_A.location, _A.clientId)", l:)`,
+        { location: locations[0], clientId },
+      );
+    } else {
+      return locations.map(locationToItem);
+    }
+  }
+
+  referencesHandler(
+    response: Response,
+  ): Item<ActionData>[] {
+    return response.flatMap(({ result }) => {
+      /**
+       * https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_references
+       */
+      const locations = result as Location[];
+      return locations;
+    }).filter((location) => !isDenoUriWithFragment(location)).map(
+      locationToItem,
+    );
+  }
+
   params(): Params {
     return {
       method: "",
+      autoJump: false,
     };
   }
 }
