fbc3e123e118295e370240cf551ef0ca131e5963
uga-rosa <uga6603@gmail.com>
2023-06-06 10:25:48 +0900

    Enum

--- denops/ddu_source_lsp/client.ts
+++ denops/ddu_source_lsp/client.ts
@@ -1,11 +1,11 @@
-export const CLIENT_NAME = {
-  "nvim-lsp": "nvim-lsp",
-  "coc.nvim": "coc.nvim",
-  "vim-lsp": "vim-lsp",
-} as const satisfies Record<string, string>;
+export const CLIENT_NAME = [
+  "nvim-lsp",
+  "coc.nvim",
+  "vim-lsp",
+] as const satisfies readonly string[];
 
-export type ClientName = typeof CLIENT_NAME[keyof typeof CLIENT_NAME];
+export type ClientName = typeof CLIENT_NAME[number];
 
 export function isClientName(clientName: string): clientName is ClientName {
-  return Object.values(CLIENT_NAME).some((name) => clientName === name);
+  return CLIENT_NAME.some((name) => clientName === name);
 }
