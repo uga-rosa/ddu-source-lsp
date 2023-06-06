e3f76a8982500528fe4c32e4bd146c56cbb0f499
uga-rosa <uga6603@gmail.com>
2023-06-06 10:23:03 +0900

    Rename kind

--- denops/@ddu-kinds/lsp.ts
+++ denops/@ddu-kinds/lsp.ts
@@ -1,13 +1,7 @@
-import {
-  ActionFlags,
-  Actions,
-  BaseKind,
-  DduItem,
-  PreviewContext,
-  Previewer,
-} from "https://deno.land/x/ddu_vim@v2.9.2/types.ts";
+import { ActionFlags, Actions, BaseKind, DduItem, Previewer } from "https://deno.land/x/ddu_vim@v2.9.2/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
 import { Range } from "npm:vscode-languageserver-types@3.17.4-next.0";
+import { asyncFlatMap } from "../ddu_source_lsp/util.ts";
 
 export type ActionData =
   & (
@@ -16,15 +10,24 @@ export type ActionData =
   )
   & {
     range?: Range;
-    resolve?: () => Promise<Range | undefined>;
+    resolveRange?: () => Promise<Range | undefined>;
+    resolvePath?: (path: string) => Promise<void> | void;
   };
 
-async function getRange(action: ActionData): Promise<Range | undefined> {
-  if (!action.range && action.resolve) {
-    action.range = await action.resolve();
-    action.resolve = undefined;
+async function getAction(item: DduItem) {
+  const action = item.action as ActionData;
+  if (!action) {
+    return;
   }
-  return action.range;
+  if (action.resolveRange) {
+    action.range = await action.resolveRange();
+    action.resolveRange = undefined;
+  }
+  if (action.resolvePath && action.path) {
+    await action.resolvePath(action.path);
+    action.resolvePath = undefined;
+  }
+  return action;
 }
 
 type OpenParams = {
@@ -57,10 +60,11 @@ export class Kind extends BaseKind<Params> {
       await denops.cmd("normal! m`");
 
       for (const item of items) {
-        const action = item?.action as ActionData;
+        const action = await getAction(item);
         if (!action) {
           continue;
         }
+
         const bufNr = action.bufNr ?? await fn.bufnr(denops, action.path);
 
         // bufnr() may return -1
@@ -83,9 +87,8 @@ export class Kind extends BaseKind<Params> {
           );
         }
 
-        const range = await getRange(action);
-        if (range) {
-          const { line, character } = range.start;
+        if (action.range) {
+          const { line, character } = action.range.start;
           const [lineNr, col] = [line + 1, character + 1];
 
           await fn.cursor(denops, lineNr, col);
@@ -104,18 +107,20 @@ export class Kind extends BaseKind<Params> {
     }) => {
       const { denops, items } = args;
 
-      const qfloclist: QuickFix[] = await Promise.all(items.map(async (item) => {
-        const action = item.action as ActionData;
-        const range = await getRange(action);
-
-        return {
-          bufnr: action.bufNr,
-          filename: action.path,
-          lnum: range ? range.start.line + 1 : undefined,
-          col: range ? range.start.character + 1 : undefined,
-          text: item.word,
-        };
-      }));
+      const qfloclist: QuickFix[] = await asyncFlatMap(items, async (item) => {
+        const action = await getAction(item);
+        if (action) {
+          return {
+            bufnr: action.bufNr,
+            filename: action.path,
+            lnum: action.range ? action.range.start.line + 1 : undefined,
+            col: action.range ? action.range.start.character + 1 : undefined,
+            text: item.word,
+          };
+        } else {
+          return [];
+        }
+      });
 
       if (qfloclist.length !== 0) {
         await fn.setqflist(denops, qfloclist);
@@ -127,27 +132,22 @@ export class Kind extends BaseKind<Params> {
   };
 
   override async getPreviewer(args: {
-    denops: Denops;
     item: DduItem;
-    actionParams: unknown;
-    previewContext: PreviewContext;
   }): Promise<Previewer | undefined> {
-    const action = args.item.action as ActionData;
+    const action = await getAction(args.item);
     if (!action) {
       return;
     }
-    const range = await getRange(action);
+
     return {
       kind: "buffer",
       expr: action.bufNr,
       path: action.path,
-      lineNr: range ? range.start.line + 1 : undefined,
+      lineNr: action.range ? action.range.start.line + 1 : undefined,
     };
   }
 
   override params(): Params {
-    return {
-      trashCommand: ["gio", "trash"],
-    };
+    return {};
   }
 }
