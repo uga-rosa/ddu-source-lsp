2019c1f18f10d8fa270c9e5b73d07525b8f476e2
uga-rosa <uga6603@gmail.com>
2023-06-11 20:57:03 +0900

    Proper position

--- denops/ddu_source_lsp/params.ts
+++ denops/ddu_source_lsp/params.ts
@@ -1,7 +1,16 @@
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
-import { Position, TextDocumentIdentifier } from "npm:vscode-languageserver-types@3.17.4-next.0";
+import {
+  CodeActionContext,
+  Position,
+  Range,
+  TextDocumentIdentifier,
+} from "npm:vscode-languageserver-types@3.17.4-next.0";
 
-import { bufNrToFileUri } from "./util.ts";
+import { getProperDiagnostics } from "../@ddu-sources/lsp_diagnostic.ts";
+import { ClientName } from "./client.ts";
+import { bufNrToFileUri, toUtfIndex } from "./util.ts";
+
+export type Encoding = "utf-8" | "utf-16" | "utf-32";
 
 export type TextDocumentPositionParams = {
   /** The text document. */
@@ -14,12 +23,14 @@ export async function makePositionParams(
   denops: Denops,
   bufNr: number,
   winId: number,
+  encodeing?: Encoding,
 ): Promise<TextDocumentPositionParams> {
-  const [_, lnum, col] = await fn.getcursorcharpos(denops, winId) as number[];
+  const [_, lnum, byteCol] = await fn.getcurpos(denops, winId) as number[];
+  const line = (await fn.getbufline(denops, bufNr, lnum))[0];
+  const character = toUtfIndex(line, byteCol - 1, encodeing);
   const position: Position = {
-    // 1-index to 0-index
     line: lnum - 1,
-    character: col - 1,
+    character,
   };
 
   return {
@@ -35,4 +46,72 @@ export async function makeTextDocumentIdentifier(
   return {
     uri: await bufNrToFileUri(denops, bufNr),
   };
+}
+
+type CodeActionParams = {
+  textDocument: TextDocumentIdentifier;
+  range: Range;
+  context: CodeActionContext;
+};
+
+export async function makeCodeActionParams(
+  clilentName: ClientName,
+  denops: Denops,
+  bufNr: number,
+  winId: number,
+  encodeing?: Encoding,
+): Promise<CodeActionParams> {
+  const textDocument = await makeTextDocumentIdentifier(denops, bufNr);
+  const range = await getSelectionRange(denops, bufNr, winId, encodeing);
+  const diagnostics = await getProperDiagnostics(clilentName, denops, bufNr);
+
+  return {
+    textDocument,
+    range,
+    context: { diagnostics: diagnostics ?? [] },
+  };
+}
+
+async function getSelectionRange(
+  denops: Denops,
+  bufNr: number,
+  winId: number,
+  encodeing?: Encoding,
+): Promise<Range> {
+  const mode = await fn.mode(denops);
+  if (mode === "v" || mode === "V") {
+    const pos1vim = await fn.getpos(denops, ".");
+    const pos2vim = await fn.getpos(denops, "v");
+    // 1-index, col is byte offset.
+    const pos1 = { lnum: pos1vim[1], col: pos1vim[2] };
+    const pos2 = { lnum: pos2vim[1], col: pos2vim[2] };
+    const [startByte, endByte] = (pos1.lnum < pos2.lnum ||
+        (pos1.lnum === pos2.lnum && pos1.col <= pos2.col))
+      ? [pos1, pos2]
+      : [pos2, pos1];
+
+    const startLine = (await fn.getbufline(denops, bufNr, startByte.lnum))[0];
+    const endLine = (await fn.getbufline(denops, bufNr, endByte.lnum))[0];
+
+    return {
+      start: {
+        line: startByte.lnum - 1,
+        character: mode === "V" ? 0 : toUtfIndex(startLine, startByte.col - 1, encodeing),
+      },
+      end: {
+        line: endByte.lnum - 1,
+        character: toUtfIndex(endLine, mode === "V" ? -1 : endByte.col - 1, encodeing),
+      },
+    };
+  } else {
+    const curpos = await fn.getcurpos(denops, winId);
+    const position: Position = {
+      line: curpos[1] - 1,
+      character: curpos[2] - 1,
+    };
+    return {
+      start: position,
+      end: position,
+    };
+  }
 }
--- denops/ddu_source_lsp/util.ts
+++ denops/ddu_source_lsp/util.ts
@@ -4,6 +4,7 @@ import { fromFileUrl, isAbsolute, toFileUrl } from "https://deno.land/std@0.190.
 import { Location, LocationLink } from "npm:vscode-languageserver-types@3.17.4-next.0";
 
 import { ActionData, ItemContext } from "../@ddu-kinds/lsp.ts";
+import { Encoding } from "./params.ts";
 
 export async function bufNrToFileUri(
   denops: Denops,
@@ -46,4 +47,132 @@ export async function asyncFlatMap<Item, Res>(
 ) {
   const a = await Promise.all(arr.map(callback));
   return a.flat();
+}
+
+const Encoder = new TextEncoder();
+function byteLength(str: string) {
+  return Encoder.encode(str).length;
+}
+
+export function toUtfIndex(
+  line: string,
+  index: number,
+  encodeing: Encoding = "utf-16",
+): number {
+  if (encodeing === "utf-8") {
+    if (index) {
+      return index;
+    } else {
+      return byteLength(line);
+    }
+  } else {
+    const { utf32Index, utf16Index } = str_utfindex(line, index);
+    if (encodeing === "utf-16") {
+      return utf16Index;
+    } else if (encodeing === "utf-32") {
+      return utf32Index;
+    } else {
+      encodeing satisfies never;
+      throw new Error(`Invalid encodeing: ${encodeing}`);
+    }
+  }
+}
+
+export function fromUtfIndex(
+  line: string,
+  index: number,
+  encodeing: Encoding = "utf-16",
+): number {
+  if (encodeing === "utf-8") {
+    if (index) {
+      return index;
+    } else {
+      return byteLength(line);
+    }
+  } else {
+    if (encodeing === "utf-16") {
+      return str_byteindex(line, index, true);
+    } else if (encodeing === "utf-32") {
+      return str_byteindex(line, index);
+    } else {
+      encodeing satisfies never;
+      throw new Error(`Invalid encodeing ${encodeing}`);
+    }
+  }
+}
+
+/**
+ * Copy of vim.str_utfindex()
+ */
+function str_utfindex(
+  str: string,
+  index: number,
+) {
+  let utf32Index = 0;
+  let utf16Index = 0;
+
+  for (let i = 0; i < index; ++i) {
+    const codePoint = str.codePointAt(i);
+    if (codePoint !== undefined) {
+      if (codePoint > 0xFFFF) {
+        // surrogate pair
+        utf16Index += 2;
+        i += 1; // Skip next unit which is the second half of a surrogate pair
+      } else {
+        utf16Index += 1;
+      }
+
+      utf32Index += 1;
+    } else {
+      // Invalid byte or embedded null, count as one code point
+      utf32Index += 1;
+      utf16Index += 1;
+    }
+  }
+
+  return { utf32Index, utf16Index };
+}
+
+/**
+ * Copy of vim.str_byteindex()
+ */
+function str_byteindex(
+  str: string,
+  index: number,
+  use_utf16 = false,
+): number {
+  let byteIndex = 0;
+  let utfIndex = 0;
+
+  for (let i = 0; i < str.length; ++i) {
+    const codePoint = str.codePointAt(i);
+    if (codePoint !== undefined) {
+      if (codePoint > 0xFFFF) {
+        // surrogate pair
+        if (use_utf16) {
+          utfIndex += 2;
+        } else {
+          utfIndex += 1;
+        }
+        i += 1; // Skip next unit which is the second half of a surrogate pair
+      } else {
+        utfIndex += 1;
+      }
+
+      if (utfIndex > index) {
+        break;
+      }
+
+      byteIndex = i + 1;
+    } else {
+      // Invalid byte or embedded null, count as one code point
+      if (utfIndex >= index) {
+        break;
+      }
+      byteIndex += 1;
+      utfIndex += 1;
+    }
+  }
+
+  return byteIndex;
 }
