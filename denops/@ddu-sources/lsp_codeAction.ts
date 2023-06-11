f02692cd68f96d7a1520c05e4200c4de6d1c77ef
uga-rosa <uga6603@gmail.com>
2023-06-11 16:37:44 +0900

    Make proper params for codeAction request

--- denops/@ddu-sources/lsp_codeAction.ts
+++ denops/@ddu-sources/lsp_codeAction.ts
@@ -1,11 +1,20 @@
 import { BaseSource, Context, DduItem, Item } from "https://deno.land/x/ddu_vim@v2.9.2/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
-import { CodeAction, Command } from "npm:vscode-languageserver-types@3.17.4-next.0";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
+import {
+  CodeAction,
+  CodeActionContext,
+  Command,
+  Diagnostic,
+  Position,
+  Range,
+  TextDocumentIdentifier,
+} from "npm:vscode-languageserver-types@3.17.4-next.0";
 
 import { lspRequest, Results } from "../ddu_source_lsp/request.ts";
 import { ClientName } from "../ddu_source_lsp/client.ts";
-import { makePositionParams } from "../ddu_source_lsp/params.ts";
+import { makeTextDocumentIdentifier } from "../ddu_source_lsp/params.ts";
 import { ActionData } from "../@ddu-kinds/lsp_codeAction.ts";
+import { getDiagnostic } from "./lsp_diagnostic.ts";
 
 type Params = {
   clientName: ClientName;
@@ -31,7 +40,7 @@ export class Source extends BaseSource<Params> {
           denops,
           ctx.bufNr,
           "textDocument/codeAction",
-          await makePositionParams(denops, ctx.bufNr, ctx.winId),
+          await makeCodeActionParams(clientName, denops, ctx.bufNr, ctx.winId),
         );
         if (results) {
           const items = codeActionsToItems(results, clientName, ctx.bufNr);
@@ -45,6 +54,68 @@ export class Source extends BaseSource<Params> {
   params(): Params {
     return {
       clientName: "nvim-lsp",
+    };
+  }
+}
+
+type CodeActionParams = {
+  textDocument: TextDocumentIdentifier;
+  range: Range;
+  context: CodeActionContext;
+};
+
+async function makeCodeActionParams(
+  clilentName: ClientName,
+  denops: Denops,
+  bufNr: number,
+  winId: number,
+): Promise<CodeActionParams> {
+  const textDocument = await makeTextDocumentIdentifier(denops, bufNr);
+  const range = await getSelectionRange(denops, bufNr, winId);
+  const dduDiagnostics = await getDiagnostic(clilentName, denops, bufNr);
+  const diagnostics: Diagnostic[] | undefined = dduDiagnostics?.map((diag) => {
+    diag.bufNr = undefined;
+    diag.path = undefined;
+    return diag;
+  });
+
+  return {
+    textDocument,
+    range,
+    context: { diagnostics: diagnostics ?? [] },
+  };
+}
+
+async function getSelectionRange(
+  denops: Denops,
+  bufNr: number,
+  winId: number,
+): Promise<Range> {
+  const mode = await fn.mode(denops);
+  if (mode === "v" || mode === "V") {
+    const pos1vim = await fn.getpos(denops, ".");
+    const pos2vim = await fn.getpos(denops, "v");
+    const pos1 = { line: pos1vim[1] - 1, character: pos1vim[2] - 1 };
+    const pos2 = { line: pos2vim[1] - 1, character: pos2vim[2] - 1 };
+    const [start, end] = (pos1.line < pos2.line ||
+        (pos1.line === pos2.line && pos1.character <= pos2.character))
+      ? [pos1, pos2]
+      : [pos2, pos1];
+    if (mode === "V") {
+      start.character = 0;
+      const endLine = (await fn.getbufline(denops, bufNr, end.line + 1))[0];
+      end.character = endLine.length - 1;
+    }
+    return { start, end };
+  } else {
+    const curpos = await fn.getcurpos(denops, winId);
+    const position: Position = {
+      line: curpos[1] - 1,
+      character: curpos[2] - 1,
+    };
+    return {
+      start: position,
+      end: position,
     };
   }
 }
