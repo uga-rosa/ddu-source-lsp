11f033afe8515d3540caaea79b921640aee50473
uga-rosa <uga6603@gmail.com>
2023-06-11 22:24:00 +0900

    Fix lsp_codeAction source

--- denops/@ddu-sources/lsp_codeAction.ts
+++ denops/@ddu-sources/lsp_codeAction.ts
@@ -1,19 +1,11 @@
 import { BaseSource, Context, DduItem, Item } from "https://deno.land/x/ddu_vim@v2.9.2/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
-import {
-  CodeAction,
-  CodeActionContext,
-  Command,
-  Position,
-  Range,
-  TextDocumentIdentifier,
-} from "npm:vscode-languageserver-types@3.17.4-next.0";
+import { Denops } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
+import { CodeAction, Command } from "npm:vscode-languageserver-types@3.17.4-next.0";
 
-import { lspRequest, Results } from "../ddu_source_lsp/request.ts";
-import { ClientName } from "../ddu_source_lsp/client.ts";
-import { makeTextDocumentIdentifier } from "../ddu_source_lsp/params.ts";
+import { lspRequest, LspResult, Method } from "../ddu_source_lsp/request.ts";
+import { Client, ClientName, getClients } from "../ddu_source_lsp/client.ts";
+import { makeCodeActionParams } from "../ddu_source_lsp/params.ts";
 import { ActionData } from "../@ddu-kinds/lsp_codeAction.ts";
-import { getProperDiagnostics } from "./lsp_diagnostic.ts";
 
 type Params = {
   clientName: ClientName;
@@ -31,21 +23,24 @@ export class Source extends BaseSource<Params> {
   }): ReadableStream<Item<ActionData>[]> {
     const { denops, sourceParams, context: ctx } = args;
     const { clientName } = sourceParams;
+    const method: Method = "textDocument/codeAction";
 
     return new ReadableStream({
       async start(controller) {
-        const results = await lspRequest(
-          clientName,
-          denops,
-          ctx.bufNr,
-          "textDocument/codeAction",
-          await makeCodeActionParams(clientName, denops, ctx.bufNr, ctx.winId),
-        );
-        if (results) {
-          const items = codeActionsToItems(results, clientName, ctx.bufNr);
-          controller.enqueue(items);
+        try {
+          const clients = await getClients(denops, clientName, ctx.bufNr);
+
+          await Promise.all(clients.map(async (client) => {
+            const params = await makeCodeActionParams(clientName, denops, ctx.bufNr, ctx.winId);
+            const result = await lspRequest(denops, client, method, params, ctx.bufNr);
+            const items = parseResult(result, client, ctx.bufNr, method);
+            controller.enqueue(items);
+          }));
+        } catch (e) {
+          console.error(e);
+        } finally {
+          controller.close();
         }
-        controller.close();
       },
     });
   }
@@ -57,87 +52,33 @@ export class Source extends BaseSource<Params> {
   }
 }
 
-type CodeActionParams = {
-  textDocument: TextDocumentIdentifier;
-  range: Range;
-  context: CodeActionContext;
-};
-
-async function makeCodeActionParams(
-  clilentName: ClientName,
-  denops: Denops,
+function parseResult(
+  result: LspResult,
+  client: Client,
   bufNr: number,
-  winId: number,
-): Promise<CodeActionParams> {
-  const textDocument = await makeTextDocumentIdentifier(denops, bufNr);
-  const range = await getSelectionRange(denops, bufNr, winId);
-  const diagnostics = await getProperDiagnostics(clilentName, denops, bufNr);
-
-  return {
-    textDocument,
-    range,
-    context: { diagnostics: diagnostics ?? [] },
-  };
-}
-
-async function getSelectionRange(
-  denops: Denops,
-  bufNr: number,
-  winId: number,
-): Promise<Range> {
-  const mode = await fn.mode(denops);
-  if (mode === "v" || mode === "V") {
-    const pos1vim = await fn.getpos(denops, ".");
-    const pos2vim = await fn.getpos(denops, "v");
-    const pos1 = { line: pos1vim[1] - 1, character: pos1vim[2] - 1 };
-    const pos2 = { line: pos2vim[1] - 1, character: pos2vim[2] - 1 };
-    const [start, end] = (pos1.line < pos2.line ||
-        (pos1.line === pos2.line && pos1.character <= pos2.character))
-      ? [pos1, pos2]
-      : [pos2, pos1];
-    if (mode === "V") {
-      start.character = 0;
-      const endLine = (await fn.getbufline(denops, bufNr, end.line + 1))[0];
-      end.character = endLine.length - 1;
-    }
-    return { start, end };
-  } else {
-    const curpos = await fn.getcurpos(denops, winId);
-    const position: Position = {
-      line: curpos[1] - 1,
-      character: curpos[2] - 1,
-    };
-    return {
-      start: position,
-      end: position,
-    };
-  }
-}
-
-function codeActionsToItems(
-  results: Results,
-  clientName: ClientName,
-  bufNr: number,
+  method: Method,
 ): Item<ActionData>[] {
-  return results.flatMap(({ result, clientId }) => {
-    /**
-     * Reference:
-     * https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_codeAction
-     */
-    const codeActions = result as (Command | CodeAction)[];
+  /**
+   * Reference:
+   * https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_codeAction
+   */
+  const codeActions = result as (Command | CodeAction)[] | null;
+  if (!codeActions) {
+    return [];
+  }
 
-    const context = { clientName, bufNr, clientId };
-    return codeActions.map((codeAction) => {
-      return {
-        word: codeAction.title,
-        action: {
-          edit: isCodeAction(codeAction) ? codeAction.edit : undefined,
-          command: isCodeAction(codeAction) ? codeAction.command : codeAction,
-          context,
-        },
-        data: codeAction,
-      };
-    });
+  const context = { client, bufNr, method };
+
+  return codeActions.map((codeAction) => {
+    return {
+      word: codeAction.title,
+      action: {
+        edit: isCodeAction(codeAction) ? codeAction.edit : undefined,
+        command: isCodeAction(codeAction) ? codeAction.command : codeAction,
+        context,
+      },
+      data: codeAction,
+    };
   });
 }
 
