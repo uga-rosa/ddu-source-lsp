16e3d8252a142a794d752d2e3ae957f8c8765e5d
uga-rosa <uga6603@gmail.com>
2023-06-14 12:51:37 +0900

    Proper handle offset encoding

--- denops/ddu_source_lsp/params.ts
+++ denops/ddu_source_lsp/params.ts
@@ -1,7 +1,16 @@
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
-import { Position, TextDocumentIdentifier } from "npm:vscode-languageserver-types@3.17.4-next.0";
+import {
+  CodeActionContext,
+  Position,
+  Range,
+  TextDocumentIdentifier,
+} from "npm:vscode-languageserver-types@3.17.4-next.0";
 
-import { bufNrToFileUri } from "./util.ts";
+import { getProperDiagnostics } from "../@ddu-sources/lsp_diagnostic.ts";
+import { Client } from "./client.ts";
+import { bufNrToFileUri, isPositionBefore } from "./util.ts";
+import { vimGetCursor, vimGetPos } from "./vim.ts";
+import { OffsetEncoding } from "./offset_encoding.ts";
 
 export type TextDocumentPositionParams = {
   /** The text document. */
@@ -14,17 +23,11 @@ export async function makePositionParams(
   denops: Denops,
   bufNr: number,
   winId: number,
+  offsetEncoding?: OffsetEncoding,
 ): Promise<TextDocumentPositionParams> {
-  const [_, lnum, col] = await fn.getcurpos(denops, winId);
-  const position: Position = {
-    // 1-index to 0-index
-    line: lnum - 1,
-    character: col - 1,
-  };
-
   return {
     textDocument: await makeTextDocumentIdentifier(denops, bufNr),
-    position,
+    position: await vimGetCursor(denops, winId, bufNr, offsetEncoding),
   };
 }
 
@@ -35,4 +38,46 @@ export async function makeTextDocumentIdentifier(
   return {
     uri: await bufNrToFileUri(denops, bufNr),
   };
+}
+
+type CodeActionParams = {
+  textDocument: TextDocumentIdentifier;
+  range: Range;
+  context: CodeActionContext;
+};
+
+export async function makeCodeActionParams(
+  denops: Denops,
+  bufNr: number,
+  clilent: Client,
+): Promise<CodeActionParams> {
+  const textDocument = await makeTextDocumentIdentifier(denops, bufNr);
+  const range = await getSelectionRange(denops, bufNr, clilent.offsetEncoding);
+  const diagnostics = await getProperDiagnostics(clilent.name, denops, bufNr);
+
+  return {
+    textDocument,
+    range,
+    context: { diagnostics: diagnostics ?? [] },
+  };
+}
+
+async function getSelectionRange(
+  denops: Denops,
+  bufNr: number,
+  offsetEncoding?: OffsetEncoding,
+): Promise<Range> {
+  // In normal mode, both 'v' and '.' mark positions will be the cursor position.
+  // In visual mode, 'v' will be the start of the visual area and '.' will be the cursor position (the end of the visual area).
+  const pos1 = await vimGetPos(denops, "v", bufNr, offsetEncoding);
+  const pos2 = await vimGetPos(denops, ".", bufNr, offsetEncoding);
+  const [start, end] = isPositionBefore(pos1, pos2) ? [pos1, pos2] : [pos2, pos1];
+
+  const mode = await fn.mode(denops);
+  if (mode === "V") {
+    start.character = 0;
+    end.character = Number.MAX_SAFE_INTEGER;
+  }
+
+  return { start, end };
 }
