61d8770d7cf6b0823ffefeced48058726d99f18e
uga-rosa <uga6603@gmail.com>
2023-06-14 12:50:51 +0900

    Better handle request

--- lua/ddu_nvim_lsp.lua
+++ lua/ddu_nvim_lsp.lua
@@ -1,44 +1,29 @@
 local M = {}
 
 ---@param bufNr integer
----@param method string
----@param params table
----@param clientId integer Valid id is greater than 0
----@return table [ok, results]
----    - ok (boolean|nil): In the case of nil, no server is attached.
----    - results (unknown[]): The results per client.
-function M.request(bufNr, method, params, clientId)
-  local filter = { bufnr = bufNr }
-  if clientId > 0 then
-    filter.id = clientId
-  end
-  local clients = vim.lsp.get_active_clients(filter)
-  if #clients == 0 then
-    return { nil }
-  end
-
-  local ok, response = pcall(vim.lsp.buf_request_sync, bufNr, method, params)
-  if not ok then
-    return { false }
-  end
-
-  local results = {}
-  for client_id, responseMessage in pairs(response) do
-    -- https://microsoft.github.io/language-server-protocol/specifications/specification-current/#responseMessage
-    local error, result = responseMessage.error, responseMessage.result
-
-    if error == nil and result then
-      table.insert(results, { clientId = client_id, result = result })
-    end
-  end
-  return { true, results }
+---@return {id: integer, offsetEncoding: string}[]
+function M.get_client_by_bufnr(bufNr)
+  local clients = vim.lsp.get_active_clients({ bufnr = bufNr })
+  return vim.tbl_map(function(client)
+    return {
+      name = "nvim-lsp",
+      id = client.id,
+      offsetEncoding = client.offset_encoding,
+    }
+  end, clients)
 end
 
----@param name string
----@return integer|nil
-function M.get_client_id_by_name(name)
-  for _, client in ipairs(vim.lsp.get_active_clients({ name = name })) do
-    return client.id
+---@param clientId integer
+---@param method string
+---@param params table
+---@param bufNr integer
+---@return unknown? result
+function M.request(clientId, method, params, bufNr)
+  local client = vim.lsp.get_client_by_id(clientId)
+  local response = client.request_sync(method, params, 5000, bufNr)
+
+  if response and response.result then
+    return response.result
   end
 end
 
