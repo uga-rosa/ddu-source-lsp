abf5433a6968021e49347e8f71c4f321e491114d
uga-rosa <uga6603@gmail.com>
2023-12-19 17:06:14 +0900

    udd

--- denops/ddu_source_lsp/deps.ts
+++ denops/ddu_source_lsp/deps.ts
@@ -1,22 +1,22 @@
-export { deadline, DeadlineError, deferred } from "https://deno.land/std@0.208.0/async/mod.ts";
+export { deadline, DeadlineError } from "https://deno.land/std@0.209.0/async/mod.ts";
 export {
   dirname,
   fromFileUrl,
   isAbsolute,
   relative,
   toFileUrl,
-} from "https://deno.land/std@0.208.0/path/mod.ts";
-export { existsSync } from "https://deno.land/std@0.208.0/fs/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.208.0/testing/asserts.ts";
-export * as uuid from "https://deno.land/std@0.208.0/uuid/mod.ts";
+} from "https://deno.land/std@0.209.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.209.0/fs/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.209.0/testing/asserts.ts";
+export * as uuid from "https://deno.land/std@0.209.0/uuid/mod.ts";
 
-export type { Denops } from "https://deno.land/x/denops_std@v5.1.0/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.1.0/function/mod.ts";
-export type { MarkInformation } from "https://deno.land/x/denops_std@v5.1.0/function/types.ts";
-export * as op from "https://deno.land/x/denops_std@v5.1.0/option/mod.ts";
-export { batch } from "https://deno.land/x/denops_std@v5.1.0/batch/mod.ts";
-export { register, unregister } from "https://deno.land/x/denops_std@v5.1.0/lambda/mod.ts";
-export { b, g } from "https://deno.land/x/denops_std@v5.1.0/variable/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.2.0/function/mod.ts";
+export type { MarkInformation } from "https://deno.land/x/denops_std@v5.2.0/function/types.ts";
+export * as op from "https://deno.land/x/denops_std@v5.2.0/option/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v5.2.0/batch/mod.ts";
+export { register, unregister } from "https://deno.land/x/denops_std@v5.2.0/lambda/mod.ts";
+export { b, g } from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
 export { test } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
 export { ensure, is } from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
 export {
@@ -28,8 +28,8 @@ export {
   applyTextEdits,
   getLines,
   setLines,
-} from "https://deno.land/x/denops_lsputil@v0.9.1/mod.ts";
-export * as lu from "https://deno.land/x/denops_lsputil@v0.9.1/mod.ts";
+} from "https://deno.land/x/denops_lsputil@v0.9.2/mod.ts";
+export * as lu from "https://deno.land/x/denops_lsputil@v0.9.2/mod.ts";
 
 export {
   ActionFlags,
@@ -45,6 +45,6 @@ export {
   type SourceOptions,
 } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
 
-export * as LSP from "npm:vscode-languageserver-types@3.17.5";
+export * as LSP from "npm:vscode-languageserver-types@3.17.6-next.1";
 
 export * as jsdiff from "https://esm.sh/diff@5.1.0";
